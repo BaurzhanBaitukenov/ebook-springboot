96acec66fe4484c153680561d51ff6b4de822511
Baurzhan Baitukenov <baitukenovb7@gmail.com>
2024-04-06 11:36:35 +0600

    first commit

---
+++ src/main/java/com/example/ebookspring/EbookSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebookspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbookSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ebookspring/EbookSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ebookspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
