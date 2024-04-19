0812170c796e835828585fdc0baa12ddfd5bb3bd
Baurzhan Baitukenov <baitukenovb7@gmail.com>
2024-04-19 19:19:46 +0500

    application properties

--- src/main/java/com/example/ebookspring/config/AppConfig.java
+++ src/main/java/com/example/ebookspring/config/AppConfig.java
@@ -34,7 +34,8 @@ public class AppConfig {
 
                         cfg.setAllowedOrigins(Arrays.asList(
                                 "http://localhost:3000",
-                                "http://localhost:4200"
+                                "http://localhost:4200",
+                                "https://ebook-vread.vercel.app"
                         ));
                         cfg.setAllowedMethods(Collections.singletonList("*"));
                         cfg.setAllowCredentials(true);
