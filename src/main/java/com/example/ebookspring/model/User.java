992dad8fffb43afdf412141c3547b62b43d8eaa9
Baurzhan Baitukenov <baitukenovb7@gmail.com>
2024-04-24 19:58:56 +0500

    added model for communication system

--- src/main/java/com/example/ebookspring/model/User.java
+++ src/main/java/com/example/ebookspring/model/User.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Table(name = "\"user\"")
 public class User {
 
+    //Ebook for user
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -48,6 +49,44 @@ public class User {
     @JsonIgnore
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL,orphanRemoval = true)
     private List<Review>reviews=new ArrayList<>();
+
+
+    //Tweet Ebook ofr user
+    private String location;
+
+    private String website;
+
+    private String birthDate;
+
+    private String image;
+
+    private String backgroundImage;
+
+    private String bio;
+
+    @Column(name = "req_user", columnDefinition = "boolean default false")
+    private boolean req_user;
+
+    @Column(name = "login_with_google", columnDefinition = "boolean default false")
+    private boolean login_with_google;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Twit> twit = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Like> likes = new ArrayList<>();
+
+    @Embedded
+    private Varification verification;
+
+    @JsonIgnore
+    @ManyToMany
+    private List<User> followers = new ArrayList<>();
+
+    @JsonIgnore
+    @ManyToMany
+    private List<User> followings = new ArrayList<>();
 
     private LocalDateTime createdAt;
 
--- src/main/java/com/example/ebookspring/repository/UserRepository.java
+++ src/main/java/com/example/ebookspring/repository/UserRepository.java
@@ -2,12 +2,16 @@ package com.example.ebookspring.repository;
 
 import com.example.ebookspring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     public User findByEmail(String email);
-
     public List<User> findAllByOrderByCreatedAtDesc();
+
+    @Query("SELECT DISTINCT u FROM User u WHERE u.firstName LIKE %:query% OR u.email LIKE %:query%")
+    List<User> searchUser(@Param("query") String query);
 }
