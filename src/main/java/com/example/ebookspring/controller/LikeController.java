4480e42660f73206c58ecc10108526a50c9be0e0
Baurzhan Baitukenov <baitukenovb7@gmail.com>
2024-04-26 11:48:20 +0500

    fixed

--- src/main/java/com/example/ebookspring/controller/LikeController.java
+++ src/main/java/com/example/ebookspring/controller/LikeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/communication")
+@RequestMapping("/api")
 public class LikeController {
 
     @Autowired
